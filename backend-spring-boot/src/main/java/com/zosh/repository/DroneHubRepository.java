ea2c018501c1fe028c45af36c7c4806a78a4785b
chinz_chinz <chinchindethw@gmail.com>
2025-11-11 13:50:12 +0700

    add geocoding method to reverse point to formatted address

---
+++ backend-spring-boot/src/main/java/com/zosh/repository/DroneHubRepository.java
@@ -0,0 +1,7 @@
+package com.zosh.repository;
+
+import com.zosh.model.DroneHub;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DroneHubRepository extends JpaRepository<DroneHub, Long> {
+}
